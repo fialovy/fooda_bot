288b20c3b3d8a34058c59b526e10fbce514ad3e3
Jeremy Lavergne <jlavergne@shoobx.com>
2019-04-05 10:12:37 -0400

    remove vecna trucks, blackify

--- src/fooda_bot.py
+++ src/fooda_bot.py
@@ -14,12 +14,12 @@ from bs4 import BeautifulSoup
 
 HERE = os.path.realpath(os.path.dirname(__file__))
 # First of all, let's be clear that this is dirty and horrible.
-warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
+warnings.filterwarnings("ignore", category=UserWarning, module="bs4")
 
-with open(HERE + '/flavor_text.yml', 'r') as fp:
+with open(HERE + "/flavor_text.yml", "r") as fp:
     FLAVOR_TEXT = yaml.load(fp)
-EMOJI_KEYWORDS = FLAVOR_TEXT['emoji_keywords']
-GREETINGS = FLAVOR_TEXT['greetings']
+EMOJI_KEYWORDS = FLAVOR_TEXT["emoji_keywords"]
+GREETINGS = FLAVOR_TEXT["greetings"]
 
 
 class FoodaEvent(object):
@@ -27,25 +27,32 @@ class FoodaEvent(object):
 
     def __init__(self, event_html):
         self.location = event_html.find(
-            'div', {'class': 'myfooda-event__meta-right myfooda-vendor-location-name'}).text.strip()
-        self.vendor = event_html.find(
-            'div', {'class': 'myfooda-event__meta-left'})
+            "div", {"class": "myfooda-event__meta-right myfooda-vendor-location-name"}
+        ).text.strip()
+        self.vendor = event_html.find("div", {"class": "myfooda-event__meta-left"})
         self.vendor_name = self.vendor.find(
-            'div', {'class': 'myfooda-event__name'}).text.strip()
+            "div", {"class": "myfooda-event__name"}
+        ).text.strip()
         self.vendor_cuisines = self.vendor.find(
-            'div', {'class': 'myfooda-event__cuisines'}).text.strip()
+            "div", {"class": "myfooda-event__cuisines"}
+        ).text.strip()
         # Possibly even add an emoji! :D
         self.cuisine_emoji = next(
-            (emoji for kw, emoji in EMOJI_KEYWORDS.items() if kw in self.vendor_cuisines.lower()),
-            None)
+            (
+                emoji
+                for kw, emoji in EMOJI_KEYWORDS.items()
+                if kw in self.vendor_cuisines.lower()
+            ),
+            None,
+        )
 
 
 def query_fooda_events(base_url, email, password):
     """Queries Fooda website for events"""
     # Sounds tricky, but turns out their user-visible login page submits
     # to a create form, and THEN redirects you.
-    login_url = '{}/create'.format(base_url)
-    referrer_url = '{}/login'.format(base_url)
+    login_url = "{}/create".format(base_url)
+    referrer_url = "{}/login".format(base_url)
     session = requests.session()
 
     # Get authenticity token for login.
@@ -57,19 +64,19 @@ def query_fooda_events(base_url, email, password):
     # Prepare login data, including token friend.
     # These are the IDs of our fields...thanks, network tab!
     login_payload = {
-        'user[email]': email,
-        'user[password]': password,
-        'authenticity_token': auth_token,
+        "user[email]": email,
+        "user[password]": password,
+        "authenticity_token": auth_token,
     }
 
     # Do teh login.
     login_result = session.post(
-        login_url, data=login_payload, headers=dict(referer=referrer_url))
+        login_url, data=login_payload, headers=dict(referer=referrer_url)
+    )
 
     # We are now on the default page for 200 Cambridge Park Drive.
     homepage_soup = BeautifulSoup(login_result.text)
-    fooda_events = homepage_soup.find_all(
-        'div', {'class': 'myfooda-event__meta'})
+    fooda_events = homepage_soup.find_all("div", {"class": "myfooda-event__meta"})
 
     for event in fooda_events:
         event = FoodaEvent(event_html=event)
@@ -78,14 +85,14 @@ def query_fooda_events(base_url, email, password):
 
 def gather_fooda_context():
     """gathers all the context needed for a fooda template"""
-    base_url = 'https://app.fooda.com/'
-    FOODA_EMAIL = os.environ['FOODA_EMAIL']
-    FOODA_PW = os.environ['FOODA_PW']
+    base_url = "https://app.fooda.com/"
+    FOODA_EMAIL = os.environ["FOODA_EMAIL"]
+    FOODA_PW = os.environ["FOODA_PW"]
 
     context = {
-        'random_greeting': random.choice(GREETINGS),
-        'current_date': datetime.date.today().strftime('%A, %d %B %Y'),
-        'fooda_events': query_fooda_events(base_url, FOODA_EMAIL, FOODA_PW),
+        "random_greeting": random.choice(GREETINGS),
+        "current_date": datetime.date.today().strftime("%A, %d %B %Y"),
+        "fooda_events": query_fooda_events(base_url, FOODA_EMAIL, FOODA_PW),
     }
     return context
 
@@ -93,12 +100,11 @@ def gather_fooda_context():
 def gather_food_trucks():
     """These are sorta hard-coded since there's no online source"""
     context = [
-        ("Little Blue Bakery", "Teri-yummy", "Zaaki Mediterranean", "Roadie's Diner",),
-        ("Little Blue Bakery", "Kebabish", "Gogi on the Block", "The Bacon Truck"),
-        ("Little Blue Bakery", "Chicken & Rice Guys", "Chicken on the Road", "SA PA"),
-        ("Little Blue Bakery", "Sate", "North East of the Border", "Moyzilla"),
-        ("Little Blue Bakery", "Zinnekens", "Compliments", "Rhythm n' Wraps",),
-
+        ("Roadie's Diner",),
+        ("Gogi on the Block", "The Bacon Truck"),
+        ("Chicken on the Road", "SA PA"),
+        ("North East of the Border", "Moyzilla"),
+        ("Zinnekens", "Compliments"),
     ]
     weekday_number = datetime.datetime.today().weekday()
     try:
@@ -120,5 +126,5 @@ def fooda_bot():
     sys.stdout.write(jinja_template.render(**context))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fooda_bot()
